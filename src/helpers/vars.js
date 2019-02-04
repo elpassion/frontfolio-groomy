bb0c78b9a78b410cdae525c93d78c9a2971ec7c8
Mariusz <insanepl@gmail.com>
2019-02-04 13:07:02 +0100

    Move Variables into one file

--- src/helpers/vars.js
+++ src/helpers/vars.js
@@ -12,3 +12,24 @@ export const colors = {
   primaryBlack: '#202020',
   black: '#000',
 };
+
+export const fontSize = {
+  xxsmall: '12px',
+  xsmall: '13px',
+  small: '14px',
+  regular: '16px',
+  medium: '18px',
+  large: '20px',
+  xlarge: '24px',
+};
+
+export const fontWeight = {
+  regular: 400,
+  bold: 700,
+};
+
+export default {
+  colors,
+  fontSize,
+  fontWeight,
+};
--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'polished';
-import { colors } from '../helpers/colors';
-import { fontWeight } from '../helpers/variables';
+import { colors, fontWeight } from '../helpers/vars';
 
 export default createGlobalStyle`
   /* Normalize */
