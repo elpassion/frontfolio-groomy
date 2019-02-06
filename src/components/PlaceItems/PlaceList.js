b999e380b5b5796161bfe41313484a8ae36e4d5f
Mariusz <insanepl@gmail.com>
2019-02-06 16:37:41 +0100

    Reformat code and remove old components

--- src/components/PlaceItems/PlaceList.js
+++ src/components/PlaceItems/PlaceList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const PlaceList = () => {
-  return <div>
-    Search Bar
-    <br />
-    Listing
-  </div>;
+  return (
+    <div>
+      Search Bar
+      <br />
+      Listing
+    </div>
+  );
 };
 
 export default PlaceList;
