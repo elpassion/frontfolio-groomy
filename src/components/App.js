6e0358c94e6227fbfd84f0cc4a29e70a9ec5576c
Gosia Heba <malgorzataheba@gmail.com>
2019-03-20 11:08:01 +0100

    Add Helmet and populate it with metadata

--- src/components/App.js
+++ src/components/App.js
@@ -5,37 +5,51 @@ import Navigation from './Navigation/Navigation';
 import Home from '../components/Views/Home';
 import Place from '../components/Views/Place';
 import GlobalStyles from '../theme/globalStyle';
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
-    <Router>
-      <React.Fragment>
-        <GlobalStyles />
-        <Route
-          render={({ location }) => {
-            return (
-              <React.Fragment>
-                <Navigation route={location} />
-
-                <TransitionGroup component={null}>
-                  <CSSTransition
-                    timeout={300}
-                    classNames='page'
-                    key={location.key}
-                  >
-                    <Switch location={location}>
-                      <Route exact path='/' component={Home} />
-                      <Route exact path='/places/:id' component={Place} />
-                      <Route component={Home} />
-                    </Switch>
-                  </CSSTransition>
-                </TransitionGroup>
-              </React.Fragment>
-            );
-          }}
+    <>
+      <Helmet
+        titleTemplate={`${process.env.REACT_APP_PAGE_TITLE} | %s`}
+        defaultTitle={process.env.REACT_APP_PAGE_TITLE}
+        htmlAttributes={{ lang: 'en' }}
+      >
+        <meta
+          name='description'
+          content='Groomy helps you find the best groomer in town, book a visit and
+          pay online - all in one mobile application'
         />
-      </React.Fragment>
-    </Router>
+      </Helmet>
+      <Router>
+        <React.Fragment>
+          <GlobalStyles />
+          <Route
+            render={({ location }) => {
+              return (
+                <React.Fragment>
+                  <Navigation route={location} />
+
+                  <TransitionGroup component={null}>
+                    <CSSTransition
+                      timeout={300}
+                      classNames='page'
+                      key={location.key}
+                    >
+                      <Switch location={location}>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/places/:id' component={Place} />
+                        <Route component={Home} />
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+                </React.Fragment>
+              );
+            }}
+          />
+        </React.Fragment>
+      </Router>
+    </>
   );
 };
 
