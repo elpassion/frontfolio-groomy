ee9d1f27934fe9fad9a983fc042cdf374af0aa76
Gosia Heba <malgorzataheba@gmail.com>
2019-03-22 17:31:36 +0100

    Update assertion descriptions in tests

--- src/data/api.test.js
+++ src/data/api.test.js
@@ -7,29 +7,29 @@ import {
 } from 'data/api';
 import { venues } from 'data/venues';
 
-it('Checking if function return object with reviews', () => {
+it('checks if function return object with reviews', () => {
   expect(typeof getReviews(1)).toBe('object');
 });
 
-it('Checking if review counter returns good value', () => {
+it('checks if review counter returns good value', () => {
   expect(typeof getReviewsCount(1)).toBe('number');
   expect(getReviewsCount(1)).toBe(1);
   expect(getReviewsCount(10)).toBe(0);
 });
 
-it('Checking rating for specific venue ID', () => {
+it('checks rating for specific venue ID', () => {
   expect(typeof getReviewsRating(1)).toBe('string');
   expect(getReviewsRating(1)).toBe('5');
   expect(getReviewsRating(1, 1)).toBe('5.0');
   expect(getReviewsRating('invalid value')).toBe('N/A');
 });
 
-it('Getting venue details', () => {
+it('pulls venue details', () => {
   expect(typeof getVenueDetails(1)).toBe('object');
   expect(getVenueDetails(1)).toBe(venues[1]);
 });
 
-it('Getting venues by term', () => {
+it('finds venues by term', () => {
   expect(typeof getPlaces('dog')).toBe('object');
   expect(typeof getPlaces()).toBe('object');
 });
--- src/helpers/invertPixelValue/test.js
+++ src/helpers/invertPixelValue/test.js
@@ -1,6 +1,6 @@
 import { invertPixelValue } from './index';
 
-it('Converting values to negative values', () => {
+it('converts values to negative values', () => {
   expect(invertPixelValue('12px')).toBe('-12px');
   expect(invertPixelValue('12rem')).toBe(null);
 });
